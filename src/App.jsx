c631fb0d17ab2a8d69677a97971d329c80fd51e1
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-11-16 21:41:33 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
-import SearchBar from "./components/SearchBar";
+/*import SearchBar from "./components/SearchBar";
 import ImageGallery from "./components/ImageGallery";
 import ImageCard from "./components/ImageCard";
 import ImageModal from "./components/ImageModal";
 import Loader from "./components/Loader";
 import LoadMoreBtn from "./components/LoadMoreBtn";
-import ErrorMessage from "./components/ErrorMessage";
+import ErrorMessage from "./components/ErrorMessage";*/
 
 const API_URL = "https://api.unsplash.com/search/photos";
 const ACCESS_KEY = "M7W80Z3OiZOdW8N73oLEE-hwTeFhqvYXu5FSMNfIxtM";
