d7a4efbb3f72d3f0d7cb34c9ed7e907bd9d6c6cc
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-11-17 15:38:44 +0100

    add

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ImageCard from "./ImageCard";
 import "./ImageGallery.css";
 
-const ImageGallery = ({ images }) => (
+const ImageGallery = ({ images, onImageClick }) => (
   <ul className="gallery">
     {images.map((image) => (
       <li key={image.id}>
-        <ImageCard image={image} className="image-item" />
+        <ImageCard
+          image={image}
+          className="image-item"
+          onClick={() => onImageClick(image)}
+        />
       </li>
     ))}
   </ul>
